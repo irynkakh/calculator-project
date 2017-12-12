c43ef60c6c7377738c07d9bc79c9616d07d333f1
Iryna <irynkakh@gmail.com>
2017-12-12 09:35:06 +0100

    hot fix main.java

--- StrangeCalculatorTask/src/main/java/strangecalculator/Main.java
+++ StrangeCalculatorTask/src/main/java/strangecalculator/Main.java
@@ -7,13 +7,7 @@ public class Main {
 
         Menu menu = new Menu();
         menu.showMenu();
-
-        // WebCalculator webCalculator = new WebCalculator();
-        //webCalculator.divide(-2.01,-2.11);
-        //    webCalculator.setup();
-        //   CalculatorPi calc = new CalculatorPi();
-        //    calc.setup();
-
+        
     }
 
 }
